cd4d74fe3a6c3be003ceb4f78b7b0fd29f895b7a
三山 <sanshan@ywwl.com>
2025-03-19 16:14:45 +0800

    feat: update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,8 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/ban-ts-comment": "off"
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-non-null-asserted-optional-chain": "off"
     }
   }
 ];
